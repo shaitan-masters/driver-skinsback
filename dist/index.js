70d4f6d9bbc582105059d58204b07d3e89a25249
Alexander Gropyanov <fasderq@gmail.com>
2021-09-15 12:50:59 +0300

    reformat code

--- dist/index.js
+++ dist/index.js
@@ -11,4 +11,5 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 __exportStar(require("./api"), exports);
+__exportStar(require("./types"), exports);
 //# sourceMappingURL=index.js.map
--- src/socket.ts
+++ src/socket.ts
@@ -1,97 +1,102 @@
 import WS from 'ws';
-import { Events } from '@osmium/events';
-import { stringify } from 'querystring';
+import {Events} from '@osmium/events';
+import {stringify} from 'querystring';
 import crypto from 'crypto';
 import {
-    SBApiConfig,
-    SBSocketData,
-    SocketState,
-    SBSocketEventName
+	SBApiConfig,
+	SBSocketData,
+	SocketState,
+	SBSocketEventName
 } from './types';
 
 
 export class SBSocket extends Events {
 
-    protected readonly config: SBApiConfig;
-    protected socket: WS;
-    protected state: SocketState = { connected: false, authed: false };
-    protected timers: { timeouts: NodeJS.Timeout[] } = { timeouts: [] };
+	protected readonly config: SBApiConfig;
+	protected socket: WS;
+	protected state: SocketState = {connected: false, authed: false};
+	protected timers: { timeouts: NodeJS.Timeout[] } = {timeouts: []};
 
-    public EVENTS: { [event in 'AUTH_SUCCESS'
-        | 'AUTH_FAILED'
-        | 'BALANCE_CHANGE'
-        | 'BUY_ITEM'
-        | 'STATUS_CHANGE']: SBSocketEventName; } = {
-            AUTH_SUCCESS: 'auth_success',
-            AUTH_FAILED: 'auth_failed',
-            BALANCE_CHANGE: 'balance_change',
-            BUY_ITEM: 'buy_item',
-            STATUS_CHANGE: 'status_change'
-        };
+	public EVENTS: {
+		[event in 'AUTH_SUCCESS'
+			| 'AUTH_FAILED'
+			| 'BALANCE_CHANGE'
+			| 'BUY_ITEM'
+			| 'STATUS_CHANGE'
+			| 'CONNECTED'
+			| 'DISCONNECTED']: SBSocketEventName | 'connected' | 'disconnected';
+	} = {
+		AUTH_SUCCESS  : 'auth_success',
+		AUTH_FAILED   : 'auth_failed',
+		BALANCE_CHANGE: 'balance_change',
+		BUY_ITEM      : 'buy_item',
+		STATUS_CHANGE : 'status_change',
+		CONNECTED     : 'connected',
+		DISCONNECTED  : 'disconnected'
+	};
 
-    constructor(config: SBApiConfig) {
-        super();
-        this.config = config;
+	constructor(config: SBApiConfig) {
+		super();
+		this.config = config;
 
-        this.connect();
-    }
+		this.connect();
+	}
 
-    protected purgeTimers(): void {
-        this.timers.timeouts.forEach(clearTimeout);
-        this.timers = { timeouts: [] };
-    }
+	protected purgeTimers(): void {
+		this.timers.timeouts.forEach(clearTimeout);
+		this.timers = {timeouts: []};
+	}
 
-    protected connect(): void {
-        this.purgeTimers();
-        this.state = { connected: false, authed: false };
+	protected connect(): void {
+		this.purgeTimers();
+		this.state = {connected: false, authed: false};
 
-        this.socket = new WS(`${this.config.socket?.endpoint}?${stringify({
-            shopid: this.config.botId,
-            signature: crypto.createHash('md5')
-                .update(`${this.config.botId}${this.config.token}`)
-                .digest('hex')
-        })}`);
+		this.socket = new WS(`${this.config.socket?.endpoint}?${stringify({
+			shopid   : this.config.botId,
+			signature: crypto.createHash('md5')
+			                 .update(`${this.config.botId}${this.config.token}`)
+			                 .digest('hex')
+		})}`);
 
-        this.socket.on('open', (): void => {
-            this.state.connected = true;
-            this.emit('connected');
-        });
+		this.socket.on('open', (): void => {
+			this.state.connected = true;
+			this.emit(this.EVENTS.CONNECTED);
+		});
 
-        this.socket.on('message', (msg: WS.Data): void => {
-            const parsed = this.parse(msg);
+		this.socket.on('message', (msg: WS.Data): void => {
+			const parsed = this.parse(msg);
 
-            if (parsed.event === this.EVENTS.AUTH_SUCCESS || parsed.event === this.EVENTS.AUTH_FAILED) {
-                this.emit(parsed.event);
-                switch (parsed.event) {
-                    case this.EVENTS.AUTH_SUCCESS:
-                        this.state.authed = true;
-                        return;
-                    case this.EVENTS.AUTH_FAILED:
-                        this.state.authed = false;
-                        this.timers.timeouts.push(setTimeout((): void => {
-                            this.emit('reconnecting');
-                            this.connect();
-                        }, this.config.socket?.options?.reconnectionTimeout || 5000));
-                        return;
-                    default:
-                        return;
-                }
-            }
+			if (parsed.event === this.EVENTS.AUTH_SUCCESS || parsed.event === this.EVENTS.AUTH_FAILED) {
+				this.emit(parsed.event);
+				switch (parsed.event) {
+					case this.EVENTS.AUTH_SUCCESS:
+						this.state.authed = true;
+						return;
+					case this.EVENTS.AUTH_FAILED:
+						this.state.authed = false;
+						this.timers.timeouts.push(setTimeout((): void => {
+							this.connect();
+						}, this.config.socket?.options?.reconnectionTimeout || 5000));
+						return;
+					default:
+						return;
+				}
+			}
 
-            parsed.data && this.emit(parsed.event, parsed.data);
-        });
+			parsed.data && this.emit(parsed.event, parsed.data);
+		});
 
-        this.socket.on('close', (): void => {
-            this.emit('disconnected');
-            this.connect();
-        });
+		this.socket.on('close', (): void => {
+			this.emit(this.EVENTS.DISCONNECTED);
+			this.connect();
+		});
 
-        this.timers.timeouts.push(setTimeout((): void => {
-            this.connect();
-        }, this.config.socket?.options?.reconnectionTimeout || 1000 * 60 * 15));
-    }
+		this.timers.timeouts.push(setTimeout((): void => {
+			this.connect();
+		}, this.config.socket?.options?.reconnectionTimeout || 1000 * 60 * 15));
+	}
 
-    protected parse(data: WS.Data): SBSocketData {
-        return JSON.parse(data.toString());
-    }
+	protected parse(data: WS.Data): SBSocketData {
+		return JSON.parse(data.toString());
+	}
 }
